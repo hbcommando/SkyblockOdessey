a37854692f68382a4a63cde36bd52087d34b0fcc
BuildTools <unconfigured@null.spigotmc.org>
2024-11-20 12:53:51 +0530

    Initial commit

---
+++ src/client/java/io/github/hbcommando/skyblockodessey/client/SkyblockodesseyClient.java
@@ -0,0 +1,10 @@
+package io.github.hbcommando.skyblockodessey.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkyblockodesseyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/io/github/hbcommando/skyblockodessey/Skyblockodessey.java
@@ -0,0 +1,10 @@
+package io.github.hbcommando.skyblockodessey;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Skyblockodessey implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
